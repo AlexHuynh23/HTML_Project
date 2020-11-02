a632350f2bf830b1acc934f79cac14117bac505b
AlexHuynh23 <70555816+AlexHuynh23@users.noreply.github.com>
2020-11-01 21:55:19 -0800

    MdBootstrap and Animations

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function locate_about(){
+  location.href = "about.html";
+}
+function locate_terms(){
+  location.href = "termsAgreement.html";
+}
+function locate_form(){
+  location.href = "form.html";
+}
+function relocate_home(){
+  location.href = "teamFinderWebsite.html";
+}
